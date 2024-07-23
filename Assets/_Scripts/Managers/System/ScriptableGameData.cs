38da720aeee427b8e8b1d030180232e5fd2bfdc2
senacakirli <senacakirli@gmail.com>
2024-07-23 22:55:16 +0300

    Revert "coin and upgrade system is initialized"
    This reverts commit 07a2b5c6cee413807bf22b6d98e9c6aab03247b6.

--- Assets/_Scripts/Managers/System/ScriptableGameData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "GameData", menuName = "ScriptableObjects/GameData")]
-
-public class ScriptableGameData : ScriptableObject
-{
-    public GameData gameData;
-}
--- Assets/_Scripts/Managers/System/ScriptableRoomData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "RoomData", menuName = "ScriptableObjects/RoomData")]
-
-public class ScriptableRoomData : ScriptableObject
-{
-    public RoomData roomData;
-}
