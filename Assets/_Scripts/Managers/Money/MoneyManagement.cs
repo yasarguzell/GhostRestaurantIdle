c2d6c9f582721a3c7c268cbce3341a9d411db83d
senacakirli <senacakirli@gmail.com>
2024-07-24 07:24:48 +0300

    money system is created

---
+++ Assets/_Scripts/Managers/Money/MoneyManagement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MoneyManagement : MonoBehaviour
+{
+    public TMP_Text booCoinCountText;
+
+    private SOMoneyData moneyDataObj;
+
+    private void Awake()
+    {
+        moneyDataObj = Resources.Load<SOMoneyData>("Datas/MoneyData");
+        booCoinCountText.text = FormatMoney(moneyDataObj.moneyData.booCoin);
+    }
+
+    public void UpdateBooCoin(int amount)
+    {
+        moneyDataObj.moneyData.booCoin += amount;
+        booCoinCountText.text = FormatMoney(moneyDataObj.moneyData.booCoin);
+    }
+
+    public static string FormatMoney(long amount)
+    {
+        if (amount >= 1000000000)
+        {
+            return (amount / 1000000000D).ToString("0.#") + "b";
+        }
+        else if (amount >= 1000000)
+        {
+            return (amount / 1000000D).ToString("0.#") + "m";
+        }
+        else if (amount >= 1000)
+        {
+            return (amount / 1000D).ToString("0.#") + "k";
+        }
+        else
+        {
+            return amount.ToString("#,0");
+        }
+    }
+}
---
+++ Assets/_Scripts/Managers/Money/SOMoneyData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MoneyData", menuName = "ScriptableObjects/MoneyData")]
+
+public class SOMoneyData : ScriptableObject
+{
+    public MoneyData moneyData;
+}
